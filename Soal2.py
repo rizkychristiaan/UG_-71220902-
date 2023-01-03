7c1abaaa91b23f6cbb24bf0866d04c7a977b26f4
rizkychristiaan <117886340+rizkychristiaan@users.noreply.github.com>
2023-01-03 12:22:50 +0700

    Add files via upload

---
+++ Soal2.py
@@ -0,0 +1,18 @@
+import turtle
+s = turtle.Screen()
+t = turtle.Turtle()
+
+t.forward(250)
+t.right(270)
+t.forward(250)
+t.right(270)
+t.forward(250)
+t.left(90)
+t.forward(250)
+#t.end poly
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(70)
+t.left(90)
+t.fordward(70)

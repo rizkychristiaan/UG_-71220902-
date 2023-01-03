fb95a941b0b1845f3c222030c344fcac25538eca
rizkychristiaan <117886340+rizkychristiaan@users.noreply.github.com>
2023-01-03 12:21:53 +0700

    Add files via upload

---
+++ Soal1k.py
@@ -0,0 +1,30 @@
+print ("kredit kreativitas mahasiwa ") 
+print ("Student Activities credit")
+print("")
+print("Pilih salah satu yang ingin di tambahakan ")
+print("1. menambah kegiatan")
+print("2.menampilkan jumblah point")
+print("3.keluar")
+print("")
+
+pilihan = int(input("masukan pilihan anda"))
+
+if pilihan = 1 
+ presentasi = float(input("masukan kategori kegiaatan organisasi"))
+ organiasi = float(input("masukan kategori kegiaatan oRgAnIsAsI"))
+ kompanitian = float(input("maukan kategori kegiantan oRgAnIsAsI"))
+ rekognisi = float(input("masukan kategori kegiatan oRgAnIsAsI"))
+elif pilihan == 1
+ presentasi = float(input("masukan kategori kegiaatan oRgAnIsAsI"))
+elif pilihan == 2 
+ organisasi = float(input("masukan kategoti kegiatan oRgAnIsAsI"))
+elif pilihan == 3 
+ kompanitian = float(input("maukan kategori kegiatan oRgAnIsAsI")) 
+elif pilihan == 4 
+ rekongnisi = float(input("masukan kategori kegiatan oRgAnIsAsI"))
+ 
+ 
+
+ 
+
+  
